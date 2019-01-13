07ebe5eeb9599dc2d9cc4c9297cb232df150f21d
Kelly Wachtel <kwachtel24@gmail.com>
2019-01-12 20:59:09 -0600

    made the navbar responsive, need to space them evenly along the bottom

--- src/components/BaseTemplate/index.js
+++ src/components/BaseTemplate/index.js
@@ -1,41 +1,53 @@
 import React from 'react';
-import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
+import Media from 'react-media';
+import {Navbar, Nav, NavItem, Glyphicon, Row,Col} from 'react-bootstrap';
 
 const BaseTemplate = (props) => {
     return (
-        <Navbar inverse collapseOnSelect>
-            <Navbar.Header>
-                <Navbar.Brand>
-                <a href="#brand">React-Bootstrap</a>
-                </Navbar.Brand>
-                <Navbar.Toggle />
-            </Navbar.Header>
-            <Navbar.Collapse>
-                <Nav>
-                <NavItem eventKey={1} href="#">
-                    Link
-                </NavItem>
-                <NavItem eventKey={2} href="#">
-                    Link
-                </NavItem>
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem divider />
-                    <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
-                </Nav>
-                <Nav pullRight>
-                <NavItem eventKey={1} href="#">
-                    Link Right
-                </NavItem>
-                <NavItem eventKey={2} href="#">
-                    Link Right
-                </NavItem>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <div>
+            <Media query="(max-width: 770px)">
+            {matches =>
+                matches ? (
+                    <Navbar inverse fixedBottom>
+                        <Nav >
+                            <NavItem eventKey={1} href="#"><Glyphicon glyph="tasks"/></NavItem>
+                            <NavItem eventKey={2} href="#"><Glyphicon glyph="usd" /></NavItem>
+                            <NavItem eventKey={3} href="#"><Glyphicon glyph="calendar" /></NavItem>
+                            <NavItem eventKey={4} href="#"><Glyphicon glyph="comment" /></NavItem>
+                            <NavItem eventKey={5} href="#"><Glyphicon glyph="cog" /></NavItem>
+                        </Nav>
+                    </Navbar>
+                ) : (
+                    <Navbar inverse fixed="top">
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                        <a href="#brand">Homie</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">
+                                <div><Glyphicon glyph="tasks" /> Tasks </div> 
+                            </NavItem>
+                            <NavItem eventKey={2} href="#">
+                                <div><Glyphicon glyph="usd" /> Splitting </div>
+                            </NavItem>
+                            <NavItem eventKey={3} href="#">
+                                <div><Glyphicon glyph="calendar" /> Calendar </div>
+                            </NavItem>
+                            <NavItem eventKey={4} href="#">
+                                <div> <Glyphicon glyph="comment" /> Messaging  </div>
+                            </NavItem>
+                            <NavItem eventKey={5} href="#">
+                                <div><Glyphicon glyph="cog" /> Settings </div>
+                            </NavItem>
+                        </Nav>
+                    </Navbar>
+                )
+            }
+            </Media>
+        </div>
+        
     );
 }
 
