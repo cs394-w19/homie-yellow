6f840fc1cfbf92953158dd8daebcc25057bf3375
Kelly Wachtel <kwachtel24@gmail.com>
2019-01-15 12:12:02 -0600

    centered mobile navbar

--- src/components/BaseTemplate/index.js
+++ src/components/BaseTemplate/index.js
@@ -8,17 +8,32 @@ const BaseTemplate = (props) => {
             <Media query="(max-width: 770px)">
             {matches =>
                 matches ? (
-                    <Navbar inverse fixedBottom>
+                    <Row className="mobile-navbar">
+                        <Col xs={7}>
+                            <Row>
+                                <Col eventKey={1} href="#" xs={4} className="mobile-item"><Glyphicon glyph="tasks" /></Col>
+                                <Col eventKey={2} href="#" xs={4} className="mobile-item"><Glyphicon glyph="usd" /></Col>
+                                <Col eventKey={3} href="#" xs={4} className="mobile-item"><Glyphicon glyph="calendar" /></Col>
+                            </Row>
+                        </Col>
+                        <Col xs={5}>
+                            <Row>
+                                <Col eventKey={4} href="#" xs={6} className="mobile-item"><Glyphicon glyph="comment" /></Col>
+                                <Col eventKey={5} href="#" xs={6} className="mobile-item"><Glyphicon glyph="cog" /></Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                    /*<Navbar inverse fixedBottom>
                         <Nav >
-                            <NavItem eventKey={1} href="#"><Glyphicon glyph="tasks"/></NavItem>
-                            <NavItem eventKey={2} href="#"><Glyphicon glyph="usd" /></NavItem>
-                            <NavItem eventKey={3} href="#"><Glyphicon glyph="calendar" /></NavItem>
-                            <NavItem eventKey={4} href="#"><Glyphicon glyph="comment" /></NavItem>
-                            <NavItem eventKey={5} href="#"><Glyphicon glyph="cog" /></NavItem>
+                            <NavItem eventKey={1} href="#" bsSize="large"><Glyphicon glyph="tasks" /></NavItem>
+                            <NavItem eventKey={2} href="#" bsSize="large"><Glyphicon glyph="usd" /></NavItem>
+                            <NavItem eventKey={3} href="#" bsSize="large"><Glyphicon glyph="calendar" /></NavItem>
+                            <NavItem eventKey={4} href="#" bsSize="large"><Glyphicon glyph="comment" /></NavItem>
+                            <NavItem eventKey={5} href="#" bsSize="large"><Glyphicon glyph="cog" /></NavItem>
                         </Nav>
-                    </Navbar>
+                    </Navbar>*/
                 ) : (
-                    <Navbar inverse fixed="top">
+                    <Navbar inverse>
                     <Navbar.Header>
                         <Navbar.Brand>
                         <a href="#brand">Homie</a>
