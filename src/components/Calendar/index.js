8e6e241243cbd5dce06d18c600b9a63482ce5638
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2019-01-21 23:37:27 -0600

    add view events and create events

--- src/components/Calendar/index.js
+++ src/components/Calendar/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import ApiCalendar from 'react-google-calendar-api';
+import ApiCalendar from 'react-google-calendar-api'; //https://www.npmjs.com/package/react-google-calendar-api
+
+// NOTE: THIS WILL NOT WORK LOCALLY!!! ALSO, probably need the API key from Nat for this to work
+// should make this way more general and probably include some stuff from this on the task tab
 
 export default class Calendar extends React.Component {
     constructor(props) {
@@ -7,10 +10,10 @@ export default class Calendar extends React.Component {
       this.handleItemClick = this.handleItemClick.bind(this);
       this.handleAddEventToCalendar = this.handleAddEventToCalendar.bind(this);
       this.signUpdate = this.signUpdate.bind(this);
-      this.handleShowCalendarEvents = this.handleAddEventToCalendar.bind(this);
-      
+      this.handleShowCalendarEvents = this.handleShowCalendarEvents.bind(this);
       
       this.state = {"calendar" : "r3ngr47kskudbj0mfinjafhh2g@group.calendar.google.com"}; // a calendar ID
+      ApiCalendar.setCalendar("r3ngr47kskudbj0mfinjafhh2g@group.calendar.google.com");
     }
 
     signUpdate(sign){
@@ -25,30 +28,42 @@ export default class Calendar extends React.Component {
       }
     }
 
-    handleAddEventToCalendar(object) {
-        ApiCalendar.createEvent(object, this.state.calendar);
-    }
-
-       /**
-     * Set the default attribute calendar
-     * @param {string} newCalendar ID.
-     */
-    setCalendar(newCalendar){
+    handleAddEventToCalendar(calEvent) {
+        console.log("tried to add event to calendar");
+        ApiCalendar.createEvent(calEvent, this.state.calendar).then((result) => {
+            console.log(result);
+              });
+        console.log("did it work??");
 
     }
+
 
     handleShowCalendarEvents(number){
+        console.log("tried to show calendar events");
+        let events = [];
         if (ApiCalendar.sign){
-            ApiCalendar.listUpcomingEvents(number).then(({result}) => {
-                console.log(result.items);
-              });
+            
+            ApiCalendar.listUpcomingEvents(number).then((result =>{
+                console.log(result);
+                events = result["result"]["items"];
+                console.log(events);
+                return events;
+            }));
+            
           } else {
               console.log("Hey somehow ApiCalendar.sign is false");
           }
+
+        console.log(events);
+        console.log("did this shit work?");
+
+        return events;
     }
 
 
     render(){
+      
+      
       return (
           <div>
             <iframe 
@@ -71,7 +86,10 @@ export default class Calendar extends React.Component {
                 sign-out
               </button>
               <button
-                  onClick={(e) => this.handleAddEventToCalendar({"name": "my birthday"})}
+                  onClick={(e) => this.handleAddEventToCalendar(
+                      {"summary": "my birthday",
+                        "end": {"dateTime": "2019-01-25T21:15:00-06:00"}, 
+                        "start": {"dateTime": "2019-01-25T20:15:00-06:00"}})}
               >
                 add event to calendar
               </button>
