6e8099958e807b23ec5219bdd18544f9dfaa4478
Kelly Wachtel <kwachtel24@gmail.com>
2019-01-24 11:51:21 -0600

    make login/logout functionality

--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
+import {Grid, Row, Button} from 'react-bootstrap';
 
 
 export default class Settings extends Component {
-
+    render() {
+        return(
+            <Grid>
+                <Row>
+                  <Button onClick={() => this.props.handleLogOut()}>Log Out </Button>
+                </Row>
+            </Grid>
+          );
+    }
 }
