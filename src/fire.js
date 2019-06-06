2c6c641973706e300d5142b67510093bfaccbd0a
Varun Ganglani <varunganglani2019@u.northwestern.edu>
2019-06-06 02:26:39 -0500

    m

--- src/fire.js
+++ src/fire.js
@@ -2,7 +2,7 @@ import firebase from 'firebase/app';
 
 var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
   apiKey: "AIzaSyDaraY-KaS5A3KTO3Kkj8tLqiTonnRVy20",
-  authDomain: "nu-lab-cats.firebaseapp.com",
+  authDomain: "homiez.firebaseapp.com",
   databaseURL: "https://homiez.firebaseio.com",
   messagingSenderId: "823560185731"
 };
